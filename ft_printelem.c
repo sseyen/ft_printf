09df3911b83a3020573ebf1981a29b9b648578c8
sseyen <krisstalexx@MacBook-Pro-Aleksandr.local>
2024-10-03 14:42:33 +0200

    Changed some names, decreased number of lines. %p and random test from fsoares need to be checked. Overall, function should be finished already

--- ft_printelem.c
+++ ft_printelem.c
@@ -6,28 +6,28 @@
 /*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 19:07:43 by alisseye          #+#    #+#             */
-/*   Updated: 2024/10/02 11:25:57 by alisseye         ###   ########.fr       */
+/*   Updated: 2024/10/03 14:40:04 by alisseye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printelem(const char *format, va_list *args)
+int	ft_printelem(const char *format, va_list *ap)
 {
 	if (*format == 'c')
-		return (ft_printchar((char)va_arg(*args, int)));
+		return (ft_printchar((char)va_arg(*ap, int)));
 	else if (*format == 's')
-		return (ft_printstr(va_arg(*args, char *)));
+		return (ft_printstr(va_arg(*ap, char *)));
 	else if (*format == 'p')
-		return (ft_printvoid(va_arg(*args, void *)));
+		return (ft_printvoid(va_arg(*ap, void *)));
 	else if (*format == 'd' || *format == 'i')
-		return (ft_printnum(va_arg(*args, int)));
+		return (ft_printnum(va_arg(*ap, int)));
 	else if (*format == 'u')
-		return (ft_printunum(va_arg(*args, unsigned int)));
+		return (ft_printunum(va_arg(*ap, unsigned int)));
 	else if (*format == 'x')
-		return (ft_printhex(va_arg(*args, unsigned int), 0));
+		return (ft_printhex(va_arg(*ap, unsigned int), 0));
 	else if (*format == 'X')
-		return (ft_printhex(va_arg(*args, unsigned int), 1));
+		return (ft_printhex(va_arg(*ap, unsigned int), 1));
 	else if (*format == '%')
 		return (ft_printchar('%'));
 	return (0);
