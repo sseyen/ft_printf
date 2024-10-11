c49c3cc69e0a9c078fbdc87553a4d5c5e25630f8
Alexandr Lisseyenko <alisseye@c2r6s6.42prague.com>
2024-10-11 19:15:09 +0200

    Fixed ft_printptr

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 18:52:30 by alisseye          #+#    #+#             */
-/*   Updated: 2024/10/02 11:32:25 by alisseye         ###   ########.fr       */
+/*   Updated: 2024/10/11 18:59:45 by alisseye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		ft_printf(const char *format, ...);
 int		ft_printelem(const char *format, va_list *args);
 int		ft_printchar(char c);
 int		ft_printstr(char *str);
-int		ft_printvoid(void *ptr);
+int		ft_printptr(void *ptr);
 int		ft_printnum(int n);
 int		ft_printunum(unsigned int n);
 int		ft_printhex(unsigned int n, int uppercase);
--- ft_printptr.c
+++ ft_printptr.c
@@ -1,22 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printvoid.c                                     :+:      :+:    :+:   */
+/*   ft_printptr.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 16:26:13 by alisseye          #+#    #+#             */
-/*   Updated: 2024/10/02 11:30:08 by alisseye         ###   ########.fr       */
+/*   Updated: 2024/10/11 19:09:50 by alisseye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printvoid(void *ptr)
+int	ft_printptr(void *ptr)
 {
-	unsigned int	address;
+	unsigned long	address;
 	int				printed;
 
+	if (!ptr)
+		return (ft_printstr("(nil)"));
 	address = (unsigned long)ptr;
 	printed = 0;
 	printed += ft_printstr("0x");
